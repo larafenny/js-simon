120d449558eae72750778c167c08534c8f7d2cb1
Lara Filippone <lara.filippone@gmail.com>
2022-03-18 15:12:19 +0100

    written step of the program

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ console.log('JS ok');
 /*Descrizione:
 Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
 Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+--------- STEP ---------
+    1. vengono visualizzati 5 numeri random per 30 sec
+    2. dopo 30 sec i numeri scompaiono
+    3. prompt per 5 voolte, l'utente deve inserire numeri uno alla volta
+    4. controllo tra numeri visualizzati e numeri inseriti
+    5. console.log('numero di numeri indovinati')
+       console.log('quali numeri indovinati')
+*/
+
+//
